9244978b3f768789ce7c544f7998b9cbbe7afe87
Malaxor <nogobaduk@gmail.com>
2018-01-23 19:47:59 -0700

    working on magnific popup function

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,23 @@
 // SERVICES SECTION
 //=====================================================
 
-$(function() {
+$(function () {
 
 	// animte on scroll
 	new WOW().init();
+});
+
+
+//=====================================================
+// WORK SECTION
+//=====================================================
+
+$(function () {
+
+	$("#work").magnificPopup({
+
+		delegate: "a", // child items selector. by clicking on it popup will open
+		type: "image"
+	});
+
 });
