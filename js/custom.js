2a341d759d72156fc0cb48215a4d493e12613369
Malaxor <nogobaduk@gmail.com>
2018-02-06 19:07:13 -0700

    started media queries (extra-small devices)

--- js/custom.js
+++ js/custom.js
@@ -65,8 +65,26 @@ $("#clients-list").owlCarousel({
 	autoplay: true,
 	autoplayHoverPause: true,
 	smartSpeed: 700,
-	loop: true
-})
+	loop: true,
+	responsive : {
+	    // breakpoint from 0 up
+	    0 : {
+	    	items: 1
+	    },
+	    // breakpoint from 480 up
+	    480 : {
+	    	items: 3
+	    },
+	    // breakpoint from 768 up
+	    768 : {
+	    	items: 5
+	    },
+	    // breakpoint from 992 up
+	    992 : {
+	    	items: 6
+	    }
+	} 
+});
 
 //=====================================================
 // NAVIGATION
